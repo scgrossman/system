55d71713fee43d1f42dec7cc9abf19263a5ec2a4
Simon Grossman <scgrossman@gmail.com>
2020-01-15 11:07:03 -0500

    fix(heading): add classname

--- packages/Heading/Heading.js
+++ packages/Heading/Heading.js
@@ -2,7 +2,7 @@ import React from "react";
 //import PropTypes from "prop-types";
 
 const Heading = ({children}) => {
-return <h1>{children}</h1>;
+return <h1 className="heading">{children}</h1>;
 }
 
 export default Heading;
