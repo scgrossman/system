6eccaa4c73084e2c69aefff2a3571586fbefaf20
Admin <park.resokp@gmail.com>
2021-03-18 14:16:25 -0400

    Change More Behaviour To More Sports

--- packages/GlobalNav/GlobalNavMobile.js
+++ packages/GlobalNav/GlobalNavMobile.js
@@ -7,14 +7,15 @@ import styles from './GlobalNav.module.scss';
 
 class GlobalNavMobile extends PureComponent {
     state = {
+        mobile_more_sports_menu_open: false,
         mobile_more_menu_open: false,
         mobile_watch_menu_open: false,
         mobile_listen_menu_open: false,
     }
 
-    toggleMobileMoreMenu = () => {
+    toggleMobileMoreSportsMenu = () => {
         this.setState({
-            mobile_more_menu_open: !this.state.mobile_more_menu_open,
+            mobile_more_sports_menu_open: !this.state.mobile_more_sports_menu_open,
         })
     }
 
@@ -27,6 +28,12 @@ class GlobalNavMobile extends PureComponent {
         window.scrollTo(0, 0)
         this.setState({
             mobile_listen_menu_open: !this.state.mobile_listen_menu_open,
+        })
+    }
+
+    toggleMobileMoreMenu = () => {
+        this.setState({
+            mobile_more_menu_open: !this.state.mobile_more_menu_open,
         })
     }
 
@@ -45,7 +52,7 @@ class GlobalNavMobile extends PureComponent {
             getReactWpUrl,
             withNavProps
         } = this.props
-        const { mobile_more_menu_open, mobile_watch_menu_open, mobile_listen_menu_open } = this.state
+        const { mobile_more_sports_menu_open, mobile_watch_menu_open, mobile_listen_menu_open, mobile_more_menu_open } = this.state
 
         const sn_now_logo_css = `.sn_now_logo_css{fill:#0098d8}`
 
@@ -67,14 +74,14 @@ class GlobalNavMobile extends PureComponent {
                     {Object.keys(nav).length !== 0 &&
                         nav.left.map(left_item => {
                             const url_formatted = left_item.url && removeDomain(left_item.url)
-                            const isMore = left_item.title === 'More'
+                            const isMoreSports = left_item.title === 'More Sports'
                             const doReactPage = isReactUrl(url_formatted)
                             const proper_url = getReactWpUrl(url_formatted, doReactPage)
 
                             return (
                                 <React.Fragment key={left_item.ID}>
                                     <li className={styles.mobile_nav_link_list_item}>
-                                        {!isMore && (
+                                        {!isMoreSports && (
                                             <LinkComponent
                                                 url={proper_url}
                                                 withNavProps={withNavProps}
@@ -91,24 +98,20 @@ class GlobalNavMobile extends PureComponent {
                                                 <div className={styles.mobile_nav_primary_title}>{left_item.title}</div>
                                             </LinkComponent>
                                         )}
-                                        {isMore && (
-                                            <div onClick={this.toggleMobileMoreMenu}>
-                                                {left_item.title}
-                                                <span
-                                                    className={`${styles.Navigation_chevron} ${
-                                                        mobile_more_menu_open ? styles.active : ''
-                                                    }`}
-                                                />
+                                        {isMoreSports && (
+                                            <div onClick={this.toggleMobileMoreSportsMenu}>
+                                                <img className={styles.mobile_nav_primary_logo} src={left_item.logo} />
+                                                <div className={styles.mobile_nav_primary_title}>{left_item.title}</div>
                                             </div>
                                         )}
                                     </li>
-                                    {mobile_more_menu_open &&
-                                        isMore &&
+                                    {mobile_more_sports_menu_open &&
+                                        isMoreSports &&
                                         left_item.child_menu.length !== 0 && (
                                             <div className={`${styles.mobile_nav_secondary_list} ${
-                                                mobile_more_menu_open ? styles.slideIn : styles.slideOut
+                                                mobile_more_sports_menu_open ? styles.slideIn : styles.slideOut
                                             }`}>
-                                                <div className={`${styles.mobile_nav_secondary_back} ${styles.Navigation_chevron}`} onClick={this.toggleMobileMoreMenu}>Back</div>
+                                                <div className={`${styles.mobile_nav_secondary_back} ${styles.Navigation_chevron}`} onClick={this.toggleMobileMoreSportsMenu}>Back</div>
                                                 {left_item.child_menu.map(more_child_item => {
                                             const url_formatted =
                                                 more_child_item.url &&
@@ -156,13 +159,14 @@ class GlobalNavMobile extends PureComponent {
                             const url_formatted = right_item.url && removeDomain(right_item.url)
                             const isWatch = right_item.title === 'Watch'
                             const isListen = right_item.title === 'Listen'
+                            const isMore = right_item.title === 'More'
                             const doReactPage = isReactUrl(url_formatted)
                             const proper_url = getReactWpUrl(url_formatted, doReactPage)
 
                             return (
                                 <React.Fragment key={right_item.ID}>
                                     <li className={`${styles.mobile_nav_link_list_item} ${styles.mobile_nav_link_list_item_right}`}>
-                                        {!isWatch && !isListen && (
+                                        {!isWatch && !isListen && !isMore &&  (
                                             <LinkComponent
                                                 url={proper_url}
                                                 withNavProps={withNavProps}
@@ -194,6 +198,16 @@ class GlobalNavMobile extends PureComponent {
                                                 <span
                                                     className={`${styles.Navigation_chevron} ${
                                                         mobile_listen_menu_open ? styles.active : ''
+                                                    }`}
+                                                />
+                                            </div>
+                                        )}
+                                        {isMore && (
+                                            <div onClick={this.toggleMobileMoreMenu}>
+                                                {right_item.title}
+                                                <span
+                                                    className={`${styles.Navigation_chevron} ${
+                                                        mobile_more_menu_open ? styles.active : ''
                                                     }`}
                                                 />
                                             </div>
@@ -285,6 +299,49 @@ class GlobalNavMobile extends PureComponent {
                                             </div>
                                         )
                                     }
+                                    {mobile_more_menu_open &&
+                                        isMore &&
+                                        right_item.child_menu.length !== 0 && (
+                                            <div className={`${styles.mobile_nav_secondary_list} ${
+                                                mobile_more_menu_open ? styles.slideIn : styles.slideOut
+                                            }`}>
+                                                <div className={`${styles.mobile_nav_secondary_back} ${styles.Navigation_chevron}`} onClick={this.toggleMobileMoreMenu}>Back</div>
+                                                {right_item.child_menu.map(more_child_item => {
+                                            const url_formatted =
+                                                more_child_item.url &&
+                                                removeDomain(more_child_item.url)
+                                            const doReactPage = isReactUrl(url_formatted)
+                                            const proper_url = getReactWpUrl(
+                                                url_formatted,
+                                                doReactPage
+                                            )
+
+                                            return (
+                                                <li
+                                                    className={`${styles.mobile_nav_link_list_item} ${styles.more}`}
+                                                    key={more_child_item.ID}
+                                                >
+                                                    <LinkComponent
+                                                        url={proper_url}
+                                                        withNavProps={withNavProps}
+                                                        singlePageNavigation={doReactPage}
+                                                        isNavLink
+                                                        onClick={() => {
+                                                            if (doReactPage) {
+                                                                navMenuToggle()
+                                                                modalMenuClose()
+                                                                window.scrollTo(0, 0)
+                                                            }
+                                                        }}
+                                                    >
+                                                        {more_child_item.title}
+                                                    </LinkComponent>
+                                                </li>
+                                            )
+                                        })}
+                                            </div>
+                                        )
+                                    }
                                 </React.Fragment>
                             )
                         })}
@@ -313,71 +370,6 @@ class GlobalNavMobile extends PureComponent {
                         </div>
                     </div>
                 </a>
-
-                {/* <ul className={styles.mobile_nav_link_list}>
-                    <li id="ump-user-account-links" className={styles.mobile_nav_link_list_item}>
-                        <button
-                            id="captureSignInLink"
-                            className="captureSignInLink capture_modal_open"
-                            style={{ display: 'block' }}
-                        >
-                            Sign In
-                        </button>
-                    </li>
-                </ul>
-
-                <div className="captureSubWrapper">
-                    <ul className={styles.mobile_nav_link_list}>
-                        <li className={styles.mobile_nav_link_list_item}>
-                            <button
-                                id="captureSignInLink"
-                                className="capture_modal_open dropdown-accordion"
-                                style={{ display: 'none' }}
-                            >
-                                Sign In
-                            </button>
-                            <div className="captureLoggedInWrapper">
-                                <div
-                                    id="captureProfileWrapper"
-                                    className="captureProfileWrapper"
-                                    style={{ display: 'none' }}
-                                >
-                                    <div
-                                        className="profile-avatar-wrapper"
-                                        style={{ border: 'none' }}
-                                    >
-                                        <img
-                                            src="#"
-                                            className="avatar avatar-24 photo profile-avatar"
-                                            height="24"
-                                            width="24"
-                                            alt="user avatar"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li className={styles.mobile_nav_link_list_item}>
-                            <a
-                                href="https://int-www.sportsnet.ca/edit-profile/"
-                                className="ump-submenu-item text-lightblue"
-                                id="captureProfileLink"
-                            >
-                                Account
-                            </a>
-                        </li>
-                        <li className={styles.mobile_nav_link_list_item}>
-                            <button
-                                id="captureSignOutLink"
-                                className="capture_end_session text-lightblue"
-                            >
-                                Sign Out
-                            </button>
-                        </li>
-                    </ul>
-                </div>
-
-                <p className="login-failed-note" style={{ display: 'none' }} /> */}
             </div>
         )
     }
